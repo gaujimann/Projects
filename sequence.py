496d4f6325b4c972eb18e72eafd9cd1d2e8bb744
Guðjón Guðjónsson <gudjong19@ru.is>
2019-09-05 11:17:08 +0000

    adding variable for new number and formulas for changeing values of all variables

--- sequence.py
+++ sequence.py
@@ -1,10 +1,12 @@
 n = int(input("Enter the length of the sequence: "))
-num = 1
+new_num = 0
 current_num = 1
 prev_num = 1
 
 for i in range(1, n + 1):
     print(current_num)
-    num = current_num
-    current_num = num + prev_num
+    new_num = current_num + prev_num
     prev_num = current_num
+    current_num = new_num
+    
+    
