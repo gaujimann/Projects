0ba65e04e97e0a0c9539e08f3a2adc266408c4f6
Guðjón Guðjónsson <gudjong19@ru.is>
2019-09-05 11:26:59 +0000

    making the program write the first 2 numbers in sequence before the "for" functions starts

--- sequence.py
+++ sequence.py
@@ -1,12 +1,17 @@
 n = int(input("Enter the length of the sequence: "))
 new_num = 0
-current_num = 1
-prev_num = 1
+current_num = 3
+prev_num1 = 2
+prev_num2 = 1
 
-for i in range(1, n + 1):
+print(prev_num2)
+print(prev_num1)
+
+for i in range(3, n + 1):
     print(current_num)
-    new_num = current_num + prev_num
-    prev_num = current_num
+    new_num = current_num + prev_num1 + prev_num2
+    prev_num2 = prev_num1
+    prev_num1 = current_num
     current_num = new_num
     
     
