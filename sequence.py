93a4e82777e2805a3498e6a4f06e6d437f260330
Guðjón Guðjónsson <gudjong19@ru.is>
2019-09-05 11:12:03 +0000

    failed attempt

--- sequence.py
+++ sequence.py
@@ -1,6 +1,10 @@
 n = int(input("Enter the length of the sequence: "))
 num = 1
-prev_num = 0
+current_num = 1
+prev_num = 1
 
 for i in range(1, n + 1):
-    print(num)
+    print(current_num)
+    num = current_num
+    current_num = num + prev_num
+    prev_num = current_num
